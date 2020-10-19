9980ed8fc929709c336b7636e84e46297a29c5d6
Bonaventure DOSSOU <43601208+bonaventuredossou@users.noreply.github.com>
2020-10-19 17:37:30 +0200

    Update fonjs.js

--- js/fonjs.js
+++ js/fonjs.js
@@ -1,13 +1,13 @@
 $(function(){
-	var $write = $('#write'),
-		shift = false,
-		capslock = false;
+	var $write = $('#write');
+	var shift = false;
+	var capslock = false;
 
 // https://www.w3schools.com/charsets/ref_utf_arrows.asp
 	
 	$('#keyboard li').click(function(){
-		var $this = $(this),
-			character = $this.html(); // If it's a lowercase letter, nothing happens to this variable
+		var $this = $(this);
+		var character = $this.html(); // If it's a lowercase letter, nothing happens to this variable
 		alert("We are here");
 	});
 });
